4393f2e1583b686949efe5debb662479e45dd752
lukaswolfrum <lukas.wolfrum@fau.de>
2023-10-09 12:42:54 +0200

    created possibility to restore password

--- auth_helpers.py
+++ auth_helpers.py
@@ -1,9 +1,53 @@
 import openai
 import streamlit as st
 import pandas as pd
-import os
+import os, string, random, smtplib
 import bcrypt
 from cryptography.fernet import Fernet
+
+def render_login_register():
+    ##
+    ## LOGIN
+    ##
+    login_tab, register_tab = st.tabs(["**Login**", "**Register**"])
+    with login_tab:
+        with st.form("login", clear_on_submit=False): 
+            username = st.text_input("Username")
+            password = st.text_input("Password",type="password")  
+            login = st.form_submit_button("Login")
+        if login:
+            login_user(username=username,password=password)
+        ##
+        ## RESTORE PASSWORD
+        ##
+        with st.expander("Forgot Password"):
+            with st.form("restore_pw",clear_on_submit=True):
+                email = st.text_input("Enter your E-Mail")
+                restore_pw  = st.form_submit_button("Restore Password")
+
+            if restore_pw:
+                with st.spinner("Sending new password."):
+                    send_new_password(email)
+    ##
+    ## REGISTER
+    ##    
+    with register_tab:
+        with st.form("register", clear_on_submit=True):   
+            st.subheader("Register")
+            email = st.text_input("E-Mail")
+            st.warning("Double check your E-Mail-Address, it is the only way to restore your account.")
+            username = st.text_input("Username")
+            apikey = st.text_input("OPENAI-API KEY",type="password")
+            password = st.text_input("Password",type="password")
+            register = st.form_submit_button("Register")
+        if register:
+            salt = bcrypt.gensalt()
+            userinfo = {"email": email.lower(),
+                        "username":username,
+                        "password": bcrypt.hashpw(password=password.encode(),salt=salt),
+                        "OPENAI_API_KEY": encrypt_api_key(apikey)
+                        }
+            create_new_user(userinfo=userinfo,check_key=False)    
 
 def check_api_key(key):
     try:
@@ -18,7 +62,7 @@ def check_api_key(key):
         return False
 
 def create_new_user(userinfo, check_key=True):
-    userdb = pd.read_json("mockup_userdb.json")
+    userdb = pd.read_json("data/mockup_userdb.json")
     #check if api key is valid, can be disabled for development purposes, set "check_key" to False
     if not check_api_key(decrypt_api_key(userinfo["OPENAI_API_KEY"])) and check_key:
         st.error("Your OPENAI API-KEY is wrong. Check again.")
@@ -29,7 +73,7 @@ def create_new_user(userinfo, check_key=True):
         st.stop()
     #add user to database
     userdb = pd.concat([userdb,pd.DataFrame(userinfo,index=[0])])     
-    userdb.to_json("mockup_userdb.json",orient="records",indent=4)
+    userdb.to_json("data/mockup_userdb.json",orient="records",indent=4)
     st.success("You registered succesfully. Login with your credentials.")
 
 def logout_user():
@@ -39,8 +83,7 @@ def logout_user():
     st.experimental_rerun()
 
 def login_user(username,password):
-    userdb = pd.read_json("mockup_userdb.json")
-    st.write(userdb)
+    userdb = pd.read_json("data/mockup_userdb.json")
     try:
         query_res = userdb[userdb["username"]==username]["password"].iloc[0]
     except IndexError:
@@ -49,38 +92,11 @@ def login_user(username,password):
     if bcrypt.checkpw(password.encode(),query_res.encode()):
         st.session_state["authentication_status"] = True
         st.session_state["username"] = username
-        st.write(userdb[userdb["username"]==st.session_state["username"]]["OPENAI_API_KEY"])
         os.environ["OPENAI_API_KEY"] = decrypt_api_key(userdb[userdb["username"]==st.session_state["username"]]["OPENAI_API_KEY"].iloc[0])
         st.experimental_rerun()
     else:
         st.error("Password is wrong.")
-
-def render_login_register():
-    login_tab, register_tab = st.tabs(["**Login**", "**Register**"])
-    with login_tab:
-        with st.form("login", clear_on_submit=False): 
-            username = st.text_input("Username")
-            password = st.text_input("Password",type="password")  
-            login = st.form_submit_button("Login")
-        if login:
-            login_user(username=username,password=password)
-
-    with register_tab:
-        with st.form("register", clear_on_submit=True):   
-            st.subheader("Register")
-            email = st.text_input("E-Mail")
-            username = st.text_input("Username")
-            apikey = st.text_input("OPENAI-API KEY",type="password")
-            password = st.text_input("Password",type="password")
-            register = st.form_submit_button("Register")
-        if register:
-            salt = bcrypt.gensalt()
-            userinfo = {"email": email,
-                        "username":username,
-                        "password": bcrypt.hashpw(password=password.encode(),salt=salt),
-                        "OPENAI_API_KEY": encrypt_api_key(apikey)
-                        }
-            create_new_user(userinfo=userinfo,check_key=False)       
+   
 
 def decrypt_api_key(encrypted_api_key):
     encryption_key = st.secrets["encryption_key"]
@@ -90,4 +106,55 @@ def decrypt_api_key(encrypted_api_key):
 def encrypt_api_key(raw_api_key):
     encryption_key = st.secrets["encryption_key"]
     fernet = Fernet(encryption_key)
-    return fernet.encrypt(raw_api_key.encode())                               
+    return fernet.encrypt(raw_api_key.encode())
+
+def send_email(recipient, generated_pw):
+    try:
+            
+        subject = "Your new Slidechatter Password"
+        text = f"""
+        
+        Hey {recipient["username"]},
+        
+        here is your new slidechatter password, make sure to change it after login in:
+
+        Username: {recipient["username"]}
+        New Password: {generated_pw}
+
+        Regards,
+        The Slidechatter Team
+
+
+        """
+        message = 'Subject: {}\n{}'.format(subject, text)
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+            smtp_server.login(st.secrets["gmail"], st.secrets["gmail_pw"])
+            smtp_server.sendmail(st.secrets["gmail"], recipient["email"], message)
+        return True
+    except:
+        return None
+
+# function from streamlit authenticator (https://github.com/mkhorasani/Streamlit-Authenticator) 
+def generate_random_pw(length: int=16) -> str:
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(length)).replace(' ','')
+ 
+def send_new_password(email):
+    userdb = pd.read_json("data/mockup_userdb.json")
+    try:
+        recipient = {"email":userdb[userdb["email"]==email.lower()]["email"].iloc[0],
+                        "username": userdb[userdb["email"]==email.lower()]["username"].iloc[0]}
+    except:
+        st.error("E-Mail not correct.")
+        st.stop()
+    new_pw = generate_random_pw()
+    if send_email(recipient=recipient,generated_pw=new_pw):
+        salt = bcrypt.gensalt()
+        userdb.loc[userdb.email == recipient["email"], "password"] = bcrypt.hashpw(password=new_pw.encode(),salt=salt)
+        userdb.to_json("data/mockup_userdb.json",orient="records",indent=4)
+        st.success("Succesfully send new password.")
+        return True
+    else:
+        print("Error sending new password.")
+        return None
+    
--- authorization_mockup.py
+++ authorization_mockup.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 from auth_helpers import *
-import os 
-import bcrypt
+from initialize import *
 
 st.title("Chat with Lecture Slides 💬")
 
@@ -12,7 +11,7 @@ if "authentication_status" not in st.session_state:
 if "username" not in st.session_state:
     st.session_state["username"] = None
 
-userdb = pd.read_json("mockup_userdb.json")
+userdb = pd.read_json("data/mockup_userdb.json")
 ###
 # Login/Register FRONTEND
 ###
@@ -27,7 +26,22 @@ if st.session_state["authentication_status"] is True:
         logout_user()
     #logout = st.sidebar.button("Logout")
     st.subheader(f"Hey {st.session_state['username']} 👋")
-
+    
     st.subheader("REST OF FORNTEND GOES HERE")
 
+    # initialize_session_state()
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] =  [{"role": "assistant", "content": 
+                                        f"How can I help you?"}]
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        
+        mockup_msg = "This is a mockup message."
+        st.session_state.messages.append({"role": "assistant", "content": mockup_msg})
+        st.chat_message("assistant").write(mockup_msg)
 
