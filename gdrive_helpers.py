ae7a45e7a1bb6c2877c300ef67af759b837d80ec
lukaswolfrum <lukas.wolfrum@fau.de>
2023-11-13 13:19:57 +0100

    updated create user function to check API Key for validity

--- gdrive_helpers.py
+++ gdrive_helpers.py
@@ -44,13 +44,9 @@ def download_faiss_data(username: str, lecture: str):
     ids = db.query("SELECT index_faiss_id,index_pkl_id from filestorage WHERE username = %s AND lecture = %s",(username,lecture))[0]
     if not os.path.isdir(f"tmp/{lecture}"):
         os.makedirs(f"tmp/{lecture}")
-    st.write(lecture)
     if not os.path.isfile(f"tmp/{lecture}/index.faiss"):
         for id, file_kind in zip(ids, ["faiss","pkl"]):
             file = drive.CreateFile({'id': id})
-            # if file_kind == "pdf":
-            #     file.GetContentFile("tmp/pdf.pdf")
-            # else:
             file.GetContentFile(f"tmp/{lecture}/index.{file_kind}")
     else:
         return True
