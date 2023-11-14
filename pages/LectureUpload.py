56a9f04d9915fd31ec478b1edd2b7be2f0c44bbe
lukaswolfrum <lukas.wolfrum@fau.de>
2023-11-14 12:00:39 +0100

    added warning for lecture reupload, changed pdf viewer from buggy pdf displaying to pdf download

--- pages/LectureUpload.py
+++ pages/LectureUpload.py
@@ -2,7 +2,9 @@ import streamlit as st
 from auth_helpers import check_login
 from initialize import load_lecturenames
 from gdrive_helpers import upload_lecture_to_drive
-from upload_helpers import save_uploadedfile, create_faiss_store,delete_lecture
+from upload_helpers import *
+
+
 
 if check_login():
     st.header("Lecture Upload")
@@ -16,22 +18,29 @@ if check_login():
         if not lecturename_upload.isalnum():
             st.error("Lecturenames must only contain letters and numbers.")
             st.stop()
+        if pdf == None:
+            st.error("Upload a pdf")
+            st.stop()
         save_uploadedfile(pdf,
                         lecturename=lecturename_upload)
 
         with st.spinner("Processing.."):
             with st.empty():
-                st.info("Creating Embeddings")
-                create_faiss_store(f"tmp/uploaded_files/{lecturename_upload}.pdf")
+                if not check_if_lecture_exists(lecturename_upload):
+                    st.info("Creating Embeddings")
+                    create_faiss_store(f"tmp/uploaded_files/{lecturename_upload}.pdf")
 
-                st.info("Uploading Data")
-                upload_lecture_to_drive(st.session_state["username"],
-                                    lecture=lecturename_upload)
-                load_lecturenames()
-                st.success("Upload successful")
+                    st.info("Uploading Data")
+                    upload_lecture_to_drive(st.session_state["username"],
+                                        lecture=lecturename_upload)
+                    load_lecturenames()
+                    st.success("Upload successful")
+                else:
+                    st.error("Lecturename already taken, choose a different one. If you want to overwrite the lecture. Delete it first.")
                 
     with st.expander("Delete Lecture",expanded=False):
         with st.form("lecture_delete"):
+            st.warning("Deleting the lecture will also delete your chat history with the lecture.")
             lecturename = st.selectbox("Select the lecture you want to delete.", options=st.session_state["lecture_list"])
             submit_delete = st.form_submit_button("Delete")
 
